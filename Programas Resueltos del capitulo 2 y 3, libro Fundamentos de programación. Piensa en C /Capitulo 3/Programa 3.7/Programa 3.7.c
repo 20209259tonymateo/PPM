87968f9fd15ee28a4937c5b4153b8da52e788afe
20209259tonymateo <134245237+20209259tonymateo@users.noreply.github.com>
2023-06-07 15:26:20 -0400

    Create Programa 3.7.c

---
+++ Programas Resueltos del capitulo 2 y 3, libro Fundamentos de programación. Piensa en C /Capitulo 3/Programa 3.7/Programa 3.7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* Lanzamiento de martillo.
+El programa, al recibir como dato N lanzamientos de martillo, calcula el promedio
+de los lanzamientos de la atleta cubana.
+I, N: variables de tipo entero.
+LAN, SLA: variables de tipo real. */
+
+int main (){
+	int i, n;
+	float lan, sla = 0;
+	
+	do
+	{
+		printf("Ingrese el numero de lanzamientos:\t");
+		scanf("%d", &n);
+	}
+	
+	while (n < 1 || n > 11);
+	/* Se utiliza la estructura do-while para verificar que el valor de N sea
+	correcto. */
+	
+	for (i = 1; i <= n; i++){
+		printf("\nIngrese el lanzamiento %d: ", i);
+		scanf("%f", &lan);
+		sla = sla + lan;
+	}
+	sla = sla / n;
+	printf("\nEl promedio de lanzamiento es: %j.2f", sla);
+}
