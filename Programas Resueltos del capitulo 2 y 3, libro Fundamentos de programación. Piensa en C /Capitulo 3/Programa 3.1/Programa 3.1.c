877d25bde8b03a7a68dce9cfb6dd7c7039c76269
20209259tonymateo <134245237+20209259tonymateo@users.noreply.github.com>
2023-06-07 14:37:02 -0400

    Create Programa 3.1.c

---
+++ Programas Resueltos del capitulo 2 y 3, libro Fundamentos de programación. Piensa en C /Capitulo 3/Programa 3.1/Programa 3.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Nómina.
+El programa, al recibir los salarios de 15 profesores, obtiene el total de la
+➥nómina de la universidad.
+I: variable de tipo entero.
+SAL y NOM: variables de tipo real. */
+
+int main(){
+	int i;
+	float sal, nom;
+	nom = 0;
+	
+	for (i = 1; i <= 15; i++){
+		printf("\Ingrese el salario del profesor%d:\t", i);
+		scanf("%f", &sal);
+		nom = nom + sal;
+	}
+	
+	printf("\nEl total de la nomina es: %.2f", nom);
+	
+	return 0;
+}
