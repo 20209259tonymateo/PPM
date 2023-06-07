275b5d28304c68b8c8ab1ea3fe617337abb15977
20209259tonymateo <134245237+20209259tonymateo@users.noreply.github.com>
2023-06-07 15:35:47 -0400

    Create Programa 3.9.c

---
+++ Programas Resueltos del capitulo 2 y 3, libro Fundamentos de programación. Piensa en C /Capitulo 3/Programa 3.9/Programa 3.9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* Serie.
+El programa imprime los términos y obtiene la suma de una determinada serie.
+I, SSE y CAM: variable de tipo entero. */
+
+int main (){
+	int i = 2, cam = 1;
+	long sse = 0;
+	while (i <= 2500)
+	{
+		sse = sse + i;
+		printf("\t %d", i);
+		if (cam)
+		{
+			i += 5;
+			cam--;
+		}
+		else{
+			i += 3;
+			cam++;
+		}
+	}
+	printf("\nLa suma de la serie es: %ld", sse);
+	
+return 0;
+}
