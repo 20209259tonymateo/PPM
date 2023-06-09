1d6b29a1c484e90b40fd53581697dd14eaf6e7ac
20209259tonymateo <134245237+20209259tonymateo@users.noreply.github.com>
2023-06-09 19:12:49 -0400

    Update Programa 5.7.c

--- Todos los programas capitulo 4 y 5, libro fundamentos de  programacion. Piensa en C/Capitulo 5/Programa 5.7/Programa 5.7.c
+++ Todos los programas capitulo 4 y 5, libro fundamentos de  programacion. Piensa en C/Capitulo 5/Programa 5.7/Programa 5.7.c
@@ -1,10 +1,9 @@
-/*
 #include <stdio.h>
 
 /* Arreglo sin elementos repetidos.
 El programa, al recibir como dato un arreglo unidimensional desordenado de N
 elementos, obtiene como salida ese mismo arreglo pero sin los elementos
-repetidos. 
+repetidos. */
 
 void lectura(int *, int);                 //Prototipos de funciones
 void imprime(int *, int);
@@ -15,30 +14,27 @@ referencia. Esto, porque el tamaño del arreglo puede disminuir. */
 
 int main(){
 	int tam, arre[100];
-	int TAM, ARRE[100];
 	/* Se escribe un do-while para verificar que el tamaño del arreglo que se
-	ingresa sea correcto. 
+	ingresa sea correcto. */
 	
 	do
 	{
-		printf("Ingrese el tamaño del arreglo: ");
+		printf("Ingrese el tamano del arreglo: ");
 		scanf("%d", &tam);
 	}
 	
 	while (tam > 100 || tam < 1);
 	lectura(arre, tam);
-	elimina(arre, &tam);
 	/* Observa que el tamaño del arreglo se pasa por referencia.*/
-	imprime(ARRE, TAM);
+	imprime(arre, tam);
 }
 
 void lectura(int a[], int t)
 /* La función Lectura se utiliza para leer un arreglo unidimensional de T
-elementos de tipo entero. 
-
+elementos de tipo entero. */
 {
 	printf("\n");
-	int iM
+	int i;
 	for (i = 0; i < t; i++)
 	{
 		printf("Ingrese el elemento %d: ", i+1);
@@ -48,10 +44,33 @@ elementos de tipo entero.
 
 void imprime(int a[], int t)
 /* La función Imprime se utiliza para escribir un arreglo unidimensional, sin 
-repeticiones, de T elementos de tipo entero. 
+repeticiones, de T elementos de tipo entero. */
 {
 	int i;
 	for (i = 0; i < t; i++)
-		printf("\nA[%d]: %d", i, a)
+		printf("\nA[%d]: %d", i, a[i]);
+	}
+	
+void elimina (int a[], int *t)
+/* Esta función se utiliza para eliminar los elementos repetidos de un arreglo
+unidimensional de T elementos de tipo entero. */ 
 
-*/
+{
+	int i = 0, k, l;
+	while (i < (*t-1))
+	{
+		k = i + 1;
+		while (k <= (*t-1))
+		{
+			if (a[i] == a[k])
+			{
+				for (l = k; l < (*t-1); l++)
+					a[l] = a[l+1];
+				*t = *t -1;
+			}
+			else
+			k++;
+		}
+		i++;
+	}
+}
