defe2d117b45cd9a847a8c32b5d779245f7d766f
20209259tonymateo <134245237+20209259tonymateo@users.noreply.github.com>
2023-07-28 22:28:10 -0400

    Create Programa 3.12.c

---
+++ Programas Resueltos del capitulo 2 y 3, libro Fundamentos de programacion Piensa en C/Capitulo 3/Programa 3.12/Programa 3.12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Serie de ULAM.
+El programa, al recibir como dato un entero positivo, obtiene y escribe
+la serie de ULAM.
+NUM: variable de tipo entero. */
+
+int main (){
+	int num;
+	printf("Ingrese el numero para calcular la serie: ");
+	scanf("%d", &num);
+
+	if ( num > 0){
+		printf("\nSerie de ULAM\n");
+		printf("%d \t", num);
+		while (num != 1)
+		{
+			if (pow(-1, num) > 0)
+				num = num / 2;
+			else 
+				num = num * 3 + 1;
+			printf("%d \t", num);
+	}
+}
+else
+	printf("\n NUM debe ser un numero positivo");
+
+return 0;
+}
