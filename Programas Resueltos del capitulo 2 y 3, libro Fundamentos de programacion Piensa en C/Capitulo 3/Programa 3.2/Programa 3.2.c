eae1dc7453731efe86d552c061119639ce6ce57b
20209259tonymateo <134245237+20209259tonymateo@users.noreply.github.com>
2023-07-28 22:20:59 -0400

    Create Programa 3.2.c

---
+++ Programas Resueltos del capitulo 2 y 3, libro Fundamentos de programacion Piensa en C/Capitulo 3/Programa 3.2/Programa 3.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Suma positivos.
+El programa, al recibir como datos N números enteros, obtiene la suma de los
+enteros positivos.
+I, N, NUM, SUM: variables de tipo entero. */
+
+int main (){
+	int i, n, num, sum;
+	sum = 0;
+	printf("Ingrese el numero de datos:\t");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++){
+		printf("Ingrese el dato numero %d: \t", i);
+		scanf("%d", &num);
+		if (num > 0)
+			sum = sum + num;
+	}
+	printf("\nLa suma de los numeros positivos es: %d", sum);
+
+	return 0;
+}
