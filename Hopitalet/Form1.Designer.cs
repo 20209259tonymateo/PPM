86317bf4bea0856ed618ab444daad8ee19fd795c
20209259tonymateo <134245237+20209259tonymateo@users.noreply.github.com>
2023-08-20 15:25:18 -0400

    Add files via upload

---
+++ Hopitalet/Form1.Designer.cs
@@ -0,0 +1,429 @@
+﻿namespace Hopitalet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbtnQ1_1 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ1_2 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ1_3 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ2_3 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ2_2 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ2_1 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ3_3 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ4_3 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ4_2 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ3_2 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ3_1 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ4_1 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ5_3 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ5_2 = new System.Windows.Forms.RadioButton();
+            this.rbtnQ5_1 = new System.Windows.Forms.RadioButton();
+            this.Diagnostico = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¿Cual es su nivel de fiebre?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "¿Posee Molestia en el pecho?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "¿Que Nivel de tos seca tiene?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sufre Usted Mareos";
+            // 
+            // rbtnQ1_1
+            // 
+            this.rbtnQ1_1.AutoSize = true;
+            this.rbtnQ1_1.Location = new System.Drawing.Point(7, 7);
+            this.rbtnQ1_1.Name = "rbtnQ1_1";
+            this.rbtnQ1_1.Size = new System.Drawing.Size(43, 17);
+            this.rbtnQ1_1.TabIndex = 7;
+            this.rbtnQ1_1.Text = "Alta";
+            this.rbtnQ1_1.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ1_2
+            // 
+            this.rbtnQ1_2.AutoSize = true;
+            this.rbtnQ1_2.Location = new System.Drawing.Point(128, 7);
+            this.rbtnQ1_2.Name = "rbtnQ1_2";
+            this.rbtnQ1_2.Size = new System.Drawing.Size(54, 17);
+            this.rbtnQ1_2.TabIndex = 8;
+            this.rbtnQ1_2.TabStop = true;
+            this.rbtnQ1_2.Text = "Medio";
+            this.rbtnQ1_2.UseVisualStyleBackColor = true;
+            this.rbtnQ1_2.CheckedChanged += new System.EventHandler(this.rbtnQ1_2_CheckedChanged);
+            // 
+            // rbtnQ1_3
+            // 
+            this.rbtnQ1_3.AutoSize = true;
+            this.rbtnQ1_3.Location = new System.Drawing.Point(250, 7);
+            this.rbtnQ1_3.Name = "rbtnQ1_3";
+            this.rbtnQ1_3.Size = new System.Drawing.Size(46, 17);
+            this.rbtnQ1_3.TabIndex = 9;
+            this.rbtnQ1_3.TabStop = true;
+            this.rbtnQ1_3.Text = "Bajo";
+            this.rbtnQ1_3.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ2_3
+            // 
+            this.rbtnQ2_3.AutoSize = true;
+            this.rbtnQ2_3.Location = new System.Drawing.Point(244, 7);
+            this.rbtnQ2_3.Name = "rbtnQ2_3";
+            this.rbtnQ2_3.Size = new System.Drawing.Size(74, 17);
+            this.rbtnQ2_3.TabIndex = 12;
+            this.rbtnQ2_3.TabStop = true;
+            this.rbtnQ2_3.Text = "Para nada";
+            this.rbtnQ2_3.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ2_2
+            // 
+            this.rbtnQ2_2.AutoSize = true;
+            this.rbtnQ2_2.Location = new System.Drawing.Point(122, 7);
+            this.rbtnQ2_2.Name = "rbtnQ2_2";
+            this.rbtnQ2_2.Size = new System.Drawing.Size(75, 17);
+            this.rbtnQ2_2.TabIndex = 11;
+            this.rbtnQ2_2.TabStop = true;
+            this.rbtnQ2_2.Text = "Muy fuerte";
+            this.rbtnQ2_2.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ2_1
+            // 
+            this.rbtnQ2_1.AutoSize = true;
+            this.rbtnQ2_1.Location = new System.Drawing.Point(1, 7);
+            this.rbtnQ2_1.Name = "rbtnQ2_1";
+            this.rbtnQ2_1.Size = new System.Drawing.Size(69, 17);
+            this.rbtnQ2_1.TabIndex = 10;
+            this.rbtnQ2_1.TabStop = true;
+            this.rbtnQ2_1.Text = "Un poco ";
+            this.rbtnQ2_1.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ3_3
+            // 
+            this.rbtnQ3_3.AutoSize = true;
+            this.rbtnQ3_3.Location = new System.Drawing.Point(245, 19);
+            this.rbtnQ3_3.Name = "rbtnQ3_3";
+            this.rbtnQ3_3.Size = new System.Drawing.Size(102, 17);
+            this.rbtnQ3_3.TabIndex = 15;
+            this.rbtnQ3_3.TabStop = true;
+            this.rbtnQ3_3.Text = "Constantemente";
+            this.rbtnQ3_3.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ4_3
+            // 
+            this.rbtnQ4_3.AutoSize = true;
+            this.rbtnQ4_3.Location = new System.Drawing.Point(244, 17);
+            this.rbtnQ4_3.Name = "rbtnQ4_3";
+            this.rbtnQ4_3.Size = new System.Drawing.Size(112, 17);
+            this.rbtnQ4_3.TabIndex = 14;
+            this.rbtnQ4_3.TabStop = true;
+            this.rbtnQ4_3.Text = "No tengo tos seca";
+            this.rbtnQ4_3.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ4_2
+            // 
+            this.rbtnQ4_2.AutoSize = true;
+            this.rbtnQ4_2.Location = new System.Drawing.Point(122, 17);
+            this.rbtnQ4_2.Name = "rbtnQ4_2";
+            this.rbtnQ4_2.Size = new System.Drawing.Size(100, 17);
+            this.rbtnQ4_2.TabIndex = 13;
+            this.rbtnQ4_2.TabStop = true;
+            this.rbtnQ4_2.Text = "Si, con irritacion";
+            this.rbtnQ4_2.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ3_2
+            // 
+            this.rbtnQ3_2.AutoSize = true;
+            this.rbtnQ3_2.Location = new System.Drawing.Point(123, 19);
+            this.rbtnQ3_2.Name = "rbtnQ3_2";
+            this.rbtnQ3_2.Size = new System.Drawing.Size(113, 17);
+            this.rbtnQ3_2.TabIndex = 18;
+            this.rbtnQ3_2.TabStop = true;
+            this.rbtnQ3_2.Text = "Cada cierto tiempo";
+            this.rbtnQ3_2.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ3_1
+            // 
+            this.rbtnQ3_1.AutoSize = true;
+            this.rbtnQ3_1.Location = new System.Drawing.Point(2, 19);
+            this.rbtnQ3_1.Name = "rbtnQ3_1";
+            this.rbtnQ3_1.Size = new System.Drawing.Size(54, 17);
+            this.rbtnQ3_1.TabIndex = 17;
+            this.rbtnQ3_1.TabStop = true;
+            this.rbtnQ3_1.Text = "Ligera";
+            this.rbtnQ3_1.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ4_1
+            // 
+            this.rbtnQ4_1.AutoSize = true;
+            this.rbtnQ4_1.Location = new System.Drawing.Point(0, 17);
+            this.rbtnQ4_1.Name = "rbtnQ4_1";
+            this.rbtnQ4_1.Size = new System.Drawing.Size(56, 17);
+            this.rbtnQ4_1.TabIndex = 16;
+            this.rbtnQ4_1.TabStop = true;
+            this.rbtnQ4_1.Text = "Aguda";
+            this.rbtnQ4_1.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ5_3
+            // 
+            this.rbtnQ5_3.AutoSize = true;
+            this.rbtnQ5_3.Location = new System.Drawing.Point(224, 19);
+            this.rbtnQ5_3.Name = "rbtnQ5_3";
+            this.rbtnQ5_3.Size = new System.Drawing.Size(123, 17);
+            this.rbtnQ5_3.TabIndex = 21;
+            this.rbtnQ5_3.TabStop = true;
+            this.rbtnQ5_3.Text = "Muy poco Frecuente";
+            this.rbtnQ5_3.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ5_2
+            // 
+            this.rbtnQ5_2.AutoSize = true;
+            this.rbtnQ5_2.Location = new System.Drawing.Point(116, 19);
+            this.rbtnQ5_2.Name = "rbtnQ5_2";
+            this.rbtnQ5_2.Size = new System.Drawing.Size(57, 17);
+            this.rbtnQ5_2.TabIndex = 20;
+            this.rbtnQ5_2.TabStop = true;
+            this.rbtnQ5_2.Text = "Nunca";
+            this.rbtnQ5_2.UseVisualStyleBackColor = true;
+            // 
+            // rbtnQ5_1
+            // 
+            this.rbtnQ5_1.AutoSize = true;
+            this.rbtnQ5_1.Location = new System.Drawing.Point(6, 19);
+            this.rbtnQ5_1.Name = "rbtnQ5_1";
+            this.rbtnQ5_1.Size = new System.Drawing.Size(102, 17);
+            this.rbtnQ5_1.TabIndex = 19;
+            this.rbtnQ5_1.TabStop = true;
+            this.rbtnQ5_1.Text = "Frecuentemente";
+            this.rbtnQ5_1.UseVisualStyleBackColor = true;
+            // 
+            // Diagnostico
+            // 
+            this.Diagnostico.Location = new System.Drawing.Point(156, 381);
+            this.Diagnostico.Name = "Diagnostico";
+            this.Diagnostico.Size = new System.Drawing.Size(200, 43);
+            this.Diagnostico.TabIndex = 22;
+            this.Diagnostico.Text = "Generar Diagnóstico";
+            this.Diagnostico.UseVisualStyleBackColor = true;
+            this.Diagnostico.Click += new System.EventHandler(this.Diagnostico_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnQ1_3);
+            this.groupBox1.Controls.Add(this.rbtnQ1_2);
+            this.groupBox1.Controls.Add(this.rbtnQ1_1);
+            this.groupBox1.Location = new System.Drawing.Point(81, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(340, 32);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbtnQ2_2);
+            this.groupBox2.Controls.Add(this.rbtnQ2_1);
+            this.groupBox2.Controls.Add(this.rbtnQ2_3);
+            this.groupBox2.Location = new System.Drawing.Point(81, 137);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(340, 30);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rbtnQ3_2);
+            this.groupBox3.Controls.Add(this.rbtnQ3_1);
+            this.groupBox3.Controls.Add(this.rbtnQ3_3);
+            this.groupBox3.Location = new System.Drawing.Point(81, 186);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(349, 53);
+            this.groupBox3.TabIndex = 25;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.rbtnQ4_1);
+            this.groupBox4.Controls.Add(this.rbtnQ4_2);
+            this.groupBox4.Controls.Add(this.rbtnQ4_3);
+            this.groupBox4.Location = new System.Drawing.Point(83, 258);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(362, 40);
+            this.groupBox4.TabIndex = 26;
+            this.groupBox4.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.rbtnQ5_1);
+            this.groupBox5.Controls.Add(this.rbtnQ5_2);
+            this.groupBox5.Controls.Add(this.rbtnQ5_3);
+            this.groupBox5.Location = new System.Drawing.Point(83, 317);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(347, 46);
+            this.groupBox5.TabIndex = 27;
+            this.groupBox5.TabStop = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(198, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(108, 25);
+            this.lbl1.TabIndex = 28;
+            this.lbl1.Text = "Hospitalet";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(78, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 13);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "¿Posee dificultad para respirar?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 447);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Diagnostico);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbtnQ1_1;
+        private System.Windows.Forms.RadioButton rbtnQ1_2;
+        private System.Windows.Forms.RadioButton rbtnQ1_3;
+        private System.Windows.Forms.RadioButton rbtnQ2_3;
+        private System.Windows.Forms.RadioButton rbtnQ2_2;
+        private System.Windows.Forms.RadioButton rbtnQ2_1;
+        private System.Windows.Forms.RadioButton rbtnQ3_3;
+        private System.Windows.Forms.RadioButton rbtnQ4_3;
+        private System.Windows.Forms.RadioButton rbtnQ4_2;
+        private System.Windows.Forms.RadioButton rbtnQ3_2;
+        private System.Windows.Forms.RadioButton rbtnQ3_1;
+        private System.Windows.Forms.RadioButton rbtnQ5_3;
+        private System.Windows.Forms.RadioButton rbtnQ5_2;
+        private System.Windows.Forms.RadioButton rbtnQ5_1;
+        private System.Windows.Forms.Button Diagnostico;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton rbtnQ4_1;
+    }
+}
+
---
+++ Hopitalet/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hopitalet
+{
+    public partial class Form1 : Form
+    {
+
+        //Enfermedades
+        // (A) Bronquitis
+        // (B) Infeccion de oido
+        // (C) Sinusitis
+        // (D) Influenza
+        // (E) Anemia
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+
+        }
+
+        private void Diagnostico_Click(object sender, EventArgs e)
+        {
+
+            //Bonquitis
+            if (rbtnQ1_2.Checked == true)
+            {
+                if (rbtnQ2_3.Checked == true)
+                {
+                    if (rbtnQ3_1.Checked == true)
+                    {
+                        if (rbtnQ4_1.Checked == true)
+                        {
+                            if (rbtnQ5_2.Checked == true)
+                                MessageBox.Show("Ustes tiene  Bronquitis, Consulte a su medico");
+                        }
+                    }
+                }
+
+            }
+            
+ 
+ 
+            //Infeccion de Oido
+            if (rbtnQ1_3.Checked == true)
+            {
+                if (rbtnQ2_1.Checked == true)
+                {
+                    if (rbtnQ3_1.Checked == true)
+                    {
+                        if (rbtnQ4_3.Checked == true)
+                        {
+                            if (rbtnQ5_1.Checked == true)
+                                MessageBox.Show("Ustes tiene  Infeccion de Oido, Consulte a su medico");
+                        }
+                    }
+                }
+
+            }
+
+
+            // Sinusistis
+            if (rbtnQ1_3.Checked == true)
+            {
+                if (rbtnQ2_3.Checked == true)
+                {
+                    if (rbtnQ3_1.Checked == true)
+                    {
+                        if (rbtnQ4_1.Checked == true)
+                        {
+                            if (rbtnQ5_3.Checked == true)
+                                MessageBox.Show("Ustes tiene  Sinusitis, Consulte a su medico");
+                        }
+                    }
+                }
+
+            }
+
+
+            //Influenza
+            if (rbtnQ1_2.Checked == true)
+            {
+                if (rbtnQ2_1.Checked == true)
+                {
+                    if (rbtnQ3_2.Checked == true)
+                    {
+                        if (rbtnQ4_2.Checked == true)
+                        {
+                            if (rbtnQ5_3.Checked == true)
+                                MessageBox.Show("Ustes tiene  Influenza, Consulte a su medico");
+                        }
+                    }
+                }
+            }
+
+
+            //Anemia
+            if (rbtnQ1_3.Checked == true)
+            {
+                if (rbtnQ2_1.Checked == true)
+                {
+                    if (rbtnQ3_2.Checked == true)
+                    {
+                        if (rbtnQ4_3.Checked == true)
+                        {
+                            if (rbtnQ5_1.Checked == true)
+                                MessageBox.Show("Ustes tiene  Anemia, Consulte a su medico");
+                        }
+                    }
+                }
+ 
+            }
+
+            if (rbtnQ1_1.Checked && rbtnQ1_2.Checked && rbtnQ1_3.Checked && rbtnQ2_1.Checked && rbtnQ2_2.Checked && rbtnQ2_3.Checked && rbtnQ3_1.Checked && rbtnQ3_2.Checked && rbtnQ3_3.Checked && rbtnQ4_1.Checked && rbtnQ4_2.Checked && rbtnQ4_3.Checked && rbtnQ5_1.Checked && rbtnQ5_2.Checked && rbtnQ5_3.Checked == true)
+            {
+                MessageBox.Show("Faltan Casillas por selecionar");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbtnQ1_2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
